680e7c078f98afeccea5273fbd85f3168c4967e3
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-07-03 09:11:27 +0100

    refactor: http.Error() method to abstract WriteHeader()

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -15,28 +13,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
-// Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	// Use the Header().Set() method to add an 'Allow: POST' header to the
-	// response header map. The first parameter is the header name, and
-	// the second parameter is the header value.
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snipet..."))
 }
 
 func main() {
-	// Register the two new handler functions and corresponding URL patterns with
-	// the servemux, in exactly the same way that we did before.
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
